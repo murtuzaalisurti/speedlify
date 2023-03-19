4dda880570ee13e97bafca92fd97b642f5469fc2
murtuza <murtuza.creativity@gmail.com>
2023-03-19 22:27:28 +0530

    added syntackle

---
+++ _data/sites/syntackle.js
@@ -0,0 +1,12 @@
+module.exports = {
+    name: "Syntackle",
+    description: "A developer blog by Murtuzaali Surti",
+    options: {
+        runs: 1,
+        frequency: 1,
+        freshChrome: "site"
+    },
+    urls: [
+		"https://syntackle.live"
+	]
+}
